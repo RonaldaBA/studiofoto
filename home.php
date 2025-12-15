a9f11919133ddad2b0805cdaf6a41de1058e89d3
raihan-zain <raihanmzain@gmail.com>
2025-12-15 14:02:25 +0700

    update home lagi

--- home.php
+++ home.php
@@ -157,8 +157,8 @@ session_start();
 <!-- ===== HERO ===== -->
 <section class="hero">
   <div>
-    <h1>RichArt Photo Studio</h1>
-    <p>Framing Your Moments Perfectly</p>
+    <h1>Framing Your Moment Perfectly</h1>
+    <p>Photo studio modern dengan konsep minimalis & pencahayaan profesional</p>
     <a href="login.php" class="btn btn-success mt-4 px-5 py-2 rounded-pill">
       Booking Sekarang
     </a>
